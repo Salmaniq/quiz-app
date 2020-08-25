821fbd92176f22408a79b6677b4db3072ff2edb1
salmaniq <muzammiliqbal811@gmail.com>
2020-08-25 18:13:32 +0500

    first commit

---
+++ app/src/main/java/com/example/myapplication/AboutActivity.java
@@ -0,0 +1,10 @@
+package com.example.myapplication;
+
+import android.view.View;
+import android.widget.Button;
+
+public class AboutActivity extends MainActivity {
+            public void onClick(View v) {
+
+            }
+        };
